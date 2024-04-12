369fbb3298eaf966f880ada4665f96eca6ec1ce5
包子 <chengbaozi6@qq.com>
2024-04-12 13:30:50 +0800

    <feature> add:BagComponent BagItemModel

---
+++ Assets/Scripts_Runtime/Entity/Common/BagComponent.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class BagComponent {
+    
+    // 背包组件
+
+    // 查找物品
+    
+    // 移除物品
+    
+    // 遍历物品
+}
---
+++ Assets/Scripts_Runtime/Entity/Common/BagItemModel.cs
@@ -0,0 +1,15 @@
+public class BagItemModel {
+    // ID
+    public int id;
+
+    public string name;
+
+    // 数量
+    public int count;
+    public int maxCount;
+    // 通用
+    public bool isConsumable;
+    // 特征
+    public bool isEatale;
+    public int  eatRestoreHp;
+}
